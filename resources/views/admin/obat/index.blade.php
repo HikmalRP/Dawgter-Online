ecc7f097638e06f7f837eba9ed5ee057053cd650
HikmalRP <hikmalrifqip@gmail.com>
2025-06-23 17:29:21 +0700

    Benerin UI

--- resources/views/admin/obat/index.blade.php
+++ resources/views/admin/obat/index.blade.php
@@ -63,17 +63,6 @@
           <div class="card-header">
             <h3 class="card-title">Daftar Obat</h3>
             <a href="/admin/obat/create" class="btn btn-sm btn-info ml-2">Tambah Obat</a>
-            <div class="card-tools">
-              <div class="input-group input-group-sm" style="width: 150px;">
-                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                <div class="input-group-append">
-                  <button type="submit" class="btn btn-default">
-                    <i class="fas fa-search"></i>
-                  </button>
-                </div>
-              </div>
-            </div>
           </div>
           <!-- /.card-header -->
           <div class="card-body table-responsive p-0">
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -105,7 +105,7 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-light-primary elevation-4">
       <!-- Brand Logo -->
       <a href="#" class="brand-link">
         <img src="{{asset('lte/dist/img/DawgterOnline.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
